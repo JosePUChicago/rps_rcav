d103bba70665079f950fd51162ca7fbc82ffe0ad
JosePUChicago <josePUChicago@users.noreply.github.com>
2017-02-01 04:10:05 -0600

    Visual, routed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,15 @@
+class GamesController < ApplicationController
+  
+  def play_rock
+
+    render("games/play_rock.html.erb")
+  end
+  def play_paper
+
+    render("games/play_paper.html.erb")
+  end
+  def play_scissors
+
+    render("games/play_scissors.html.erb")
+  end
+end
